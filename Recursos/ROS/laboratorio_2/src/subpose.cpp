40da92601f44a9ab41c7980353a547339505c0d4
JSDaleman <jdaleman@unal.edu.co>
2025-04-30 18:15:52 -0500

    Fix: Se corrige formateo de los archivos de C++  del paquete de ROS

--- Recursos/ROS/laboratorio_2/src/subpose.cpp
+++ Recursos/ROS/laboratorio_2/src/subpose.cpp
@@ -6,19 +6,19 @@
 // A callback function. Executed each time a new pose message arrives.
 void poseMessageReceived(const turtlesim::Pose& msg) 
 {
-ROS_INFO_STREAM(std::setprecision(2) << std::fixed << "position=("
-<< msg.x << "," << msg.y << ")" << " direction=" << msg.theta);
+    ROS_INFO_STREAM(std::setprecision(2) << std::fixed << "position=("
+    << msg.x << "," << msg.y << ")" << " direction=" << msg.theta);
 }
 
 int main(int argc, char** argv)
 {
-// Initialize the ROS system and become a node.
-ros::init(argc, argv, "subscribe_to_pose");
-ros::NodeHandle nh;
+    // Initialize the ROS system and become a node.
+    ros::init(argc, argv, "subscribe_to_pose");
+    ros::NodeHandle nh;
 
-// Create a subscriber object.
-ros::Subscriber sub = nh.subscribe("turtle1/pose", 1000, &poseMessageReceived);
+    // Create a subscriber object.
+    ros::Subscriber sub = nh.subscribe("turtle1/pose", 1000, &poseMessageReceived);
 
-// Let ROS take over.
-ros::spin();
+    // Let ROS take over.
+    ros::spin();
 }
